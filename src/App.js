82283d30cb2c02604a6c2003df7a2068cadb76e8
Kaizher <secryuit401@naver.com>
2024-12-13 17:33:10 +0900

    App.js 연결

--- src/App.js
+++ src/App.js
@@ -10,6 +10,7 @@ import Profile from './components/Profile/profile';
 import PostPage from './components/post/post';
 import Main from './components/mainpage/main'
 import Login from './components/Login/Login';
+import Sign from './components/sign/Sign';
 
 const App = () => {
   return (
@@ -23,6 +24,7 @@ const App = () => {
         <Route path="/commitment" element={<Commitment />} />
         <Route path="/review" element={<Review />} />
         <Route path="/chat" element={<Chat />} /> 
+        <Route path="/sign" element={<Sign/>} />
         <Route path="/" element={<Around/>} />
         <Route path="/profile" element={<Profile/>} />
       </Routes>
